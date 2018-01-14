02f2c6d963f33e42e066ef0129dc11c317252718
Steve Jackson <steve@testdouble.com>
2018-01-14 12:31:30 -0500

    Add add/remove to redux

--- store/index.js
+++ store/index.js
@@ -1,19 +1,49 @@
+let todoIdGenerator = 0;
 const initialState = {
-    hasCompletedItems: false
+  hasCompletedItems: false,
+  todos: []
 };
 
+function addTodo(text) {
+  return {
+    type: 'ADD_TODO',
+    todo: { id: todoIdGenerator++, text: text }
+  }
+}
+
+function removeTodo(todo) {
+  return {
+    type: 'REMOVE_TODO',
+    todo: todo
+  }
+}
+
 function setHasCompletedItems(hasCompletedItems = false) {
-    return {
-        type: 'SET_HAS_COMPLETED_ITEMS',
-        hasCompletedItems: hasCompletedItems
-    };
+  return {
+    type: 'SET_HAS_COMPLETED_ITEMS',
+    hasCompletedItems: hasCompletedItems
+  };
 }
 
 function reducer(state = initialState, action) {
-    if (action.type = 'SET_HAS_COMPLETED_ITEMS') {
-        return { hasCompletedItems: action.hasCompletedItems };
-    }
-    return state;
+  switch(action.type) {
+    case 'SET_HAS_COMPLETED_ITEMS':
+      return Object.assign({}, state, {
+        hasCompletedItems: action.hasCompletedItems
+      });
+    case 'ADD_TODO':
+      return Object.assign({}, state, {
+        todos: state.todos.concat(action.todo)
+      });
+    case 'REMOVE_TODO':
+      return Object.assign({}, state, {
+        todos: state.todos.filter((todo) =>
+            todo.id != action.todo.id
+        )
+      });
+    default:
+      return state;
+  }
 }
 
 var store = Redux.createStore(reducer);
