686c2ff027ec754224109796239aab6c21337c10
Steve Jackson <steve@testdouble.com>
2018-01-14 09:22:20 -0500

    Move submit function to component

--- components/AddTodoInput.js
+++ components/AddTodoInput.js
@@ -1,7 +1,13 @@
 class AddTodoInput extends React.Component {
+  handleSubmit(e) {
+    e.preventDefault();
+    var input = $("input#todoInput");
+    addTodo(input.val());
+    input.val("");
+  }
   render() {
     return (
-      <form className="form-inline d-inline" id="addForm">
+      <form className="form-inline d-inline" id="addForm" onSubmit="handleSubmit">
           <input id="todoInput" className="form-control" type="text"/>
           <button id="addTodo" type="submit" className="btn btn-primary mx-1">Add</button>
       </form>
