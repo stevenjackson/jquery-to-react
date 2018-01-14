427f264e660d48aa73d92a5931dee4b79c2f88d1
Steve Jackson <steve@testdouble.com>
2018-01-14 09:35:57 -0500

    Add state to the Add todo component

--- components/AddTodoInput.js
+++ components/AddTodoInput.js
@@ -1,14 +1,23 @@
 class AddTodoInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+
+  handleInput(e) {
+    this.setState({ text: e.target.value });
+  }
+
   handleSubmit(e) {
     e.preventDefault();
-    var input = $("input#todoInput");
-    addTodo(input.val());
-    input.val("");
+    addTodo(this.state.text);
+    this.setState( {text: ""} );
   }
+
   render() {
     return (
-      <form className="form-inline d-inline" id="addForm" onSubmit="handleSubmit">
-          <input id="todoInput" className="form-control" type="text"/>
+      <form className="form-inline d-inline" id="addForm" onSubmit={this.handleSubmit.bind(this)} >
+          <input id="todoInput" className="form-control" type="text" value={this.state.text} onChange={this.handleInput.bind(this)} />
           <button id="addTodo" type="submit" className="btn btn-primary mx-1">Add</button>
       </form>
     );
