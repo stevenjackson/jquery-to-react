83895fdc9ba6a7a20afbae39b445d11783532ee6
Steve Jackson <steve@testdouble.com>
2018-01-14 13:08:24 -0500

    Refactor TodoItem to use redux state rather than component state for complete check

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,15 +1,6 @@
 class TodoItem extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      completed: false
-    };
-  }
-
   toggleComplete() {
-    const completed = !this.state.completed;
-    this.setState({ completed: completed })
-    store.dispatch(todoCompleted(this.props.todo, completed));
+    store.dispatch(todoCompleted(this.props.todo, !this.props.todo.completed));
   }
 
   delete() {
@@ -17,16 +8,17 @@ class TodoItem extends React.Component {
   }
 
   render() {
-    let flexStyle = {flex: '1 1 auto'};
-    let todoText = this.props.todo.text;
-    if (this.state.completed) {
+    const flexStyle = {flex: '1 1 auto'};
+    const todo = this.props.todo;
+    let todoText = todo.text;
+    if (todo.completed) {
       todoText = <del>{todoText}</del>
     }
     return(
        <li className='list-group-item d-flex'>
          <div className='form-check' style={flexStyle} onChange={this.toggleComplete.bind(this)}>
-           <input type='checkbox' className='form-check-input' id={this.props.todo.id} />
-           <label className='form-check-label' htmlFor={this.props.todo.id}>
+           <input type='checkbox' className='form-check-input' id={todo.id} />
+           <label className='form-check-label' htmlFor={todo.id}>
             {todoText}
            </label>
          </div>
